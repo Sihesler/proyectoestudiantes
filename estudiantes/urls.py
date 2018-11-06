f4e8135ade743bcced0e8617062c1186d2c29f71
Sihesler <siheslermateo@gmail.com>
2018-11-06 17:01:47 -0600

    CRUD Eliminar y detalle

--- estudiantes/urls.py
+++ estudiantes/urls.py
@@ -1,14 +1,18 @@
 from django.conf.urls import url
 from . import views
-from estudiantes.views import index, curso_list
+
 
 urlpatterns = [
     #url(r'^$', views.lista_peliculas, name ='lista_peliculas'),
     url(r'^$', views.index, name='index'),
-    url(r'^listar_estudiante$', views.estudiante_list, name='estudiante_list'),
+    url(r'^listar/estudiante/$', views.estudiante_list, name='estudiante_list'),
     url(r'^listar_curso$', views.curso_list, name='curso_list'),
     url(r'^nuevo$', views.curso_nuevo, name='curso_nuevo'),
     url(r'^nuevo_estudiante$', views.estudiante_nuevo, name='estudiante_nuevo'),
+    url(r'^estudiante/(?P<pk>[0-9]+)/$', views.estudiante_detalle, name='estudiante_detalle'),
+    url(r'^curso/(?P<pk>[0-9]+)/$', views.curso_detalle, name='curso_detalle'),
+    url(r'^estudiante/(?P<pk>\d+)/remove/$', views.estudiante_remove, name='estudiante_remove'),
+    url(r'^curso/(?P<pk>\d+)/remove/$', views.curso_remove, name='curso_remove'),
 
     #url(r'^detalle$', views.curso_detalle, name='curso_detalle'),
     #url(r'^listar$', views.curso_list, name='curso_list'),
--- estudiantes/views.py
+++ estudiantes/views.py
@@ -8,6 +8,14 @@ from django.contrib import messages
 def index(request):
     return render(request, 'estudiante/index.html')
 
+def estudiante_detalle(request, pk):
+        estudiante = get_object_or_404(Estudiante, pk=pk)
+        return render(request, 'estudiante/estudiante_detalle.html', {'estudiante': estudiante})
+
+def curso_detalle(request, pk):
+        curso = get_object_or_404(Curso, pk=pk)
+        return render(request, 'estudiante/curso_detalle.html', {'curso': curso})
+
 
 def estudiante_list(request):
     estudiante = Estudiante.objects.all()
@@ -45,3 +53,15 @@ def estudiante_nuevo(request):
     else:
         form = EstudianteForm()
     return render(request, 'estudiante/estudiante_nuevo.html', {'form': form})
+
+
+
+def estudiante_remove(request, pk):
+    estudiante = get_object_or_404(Estudiante, pk=pk)
+    estudiante.delete()
+    return redirect('estudiante_list')
+
+def curso_remove(request, pk):
+    curso = get_object_or_404(Curso, pk=pk)
+    curso.delete()
+    return redirect('curso_list')
