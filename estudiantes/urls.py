c52d84d6f784219766d52866d490f1e58a210146
Sihesler <siheslermateo@gmail.com>
2018-11-06 14:40:19 -0600

    Listar, datalle.. y crear un nuevo curso de muchos a muchos y crear un estudiante

--- estudiantes/urls.py
+++ estudiantes/urls.py
@@ -1,9 +1,15 @@
 from django.conf.urls import url
 from . import views
-from estudiantes.views import index
+from estudiantes.views import index, curso_list
 
 urlpatterns = [
     #url(r'^$', views.lista_peliculas, name ='lista_peliculas'),
     url(r'^$', views.index, name='index'),
+    url(r'^listar_estudiante$', views.estudiante_list, name='estudiante_list'),
+    url(r'^listar_curso$', views.curso_list, name='curso_list'),
+    url(r'^nuevo$', views.curso_nuevo, name='curso_nuevo'),
+    url(r'^nuevo_estudiante$', views.estudiante_nuevo, name='estudiante_nuevo'),
 
+    #url(r'^detalle$', views.curso_detalle, name='curso_detalle'),
+    #url(r'^listar$', views.curso_list, name='curso_list'),
 ]
--- estudiantes/views.py
+++ estudiantes/views.py
@@ -7,3 +7,41 @@ from django.contrib import messages
 
 def index(request):
     return render(request, 'estudiante/index.html')
+
+
+def estudiante_list(request):
+    estudiante = Estudiante.objects.all()
+    return render(request, 'estudiante/listar_estudiante.html', {'estudiante':estudiante})
+
+
+def curso_list(request):
+    curso = Curso.objects.all()
+    return render(request, 'estudiante/listar_cursos.html', {'curso':curso})
+
+
+
+def curso_nuevo(request):
+    if request.method == "POST":
+        formulario = CursoForm(request.POST)
+        if formulario.is_valid():
+            add=formulario.save(commit=False)
+            add.save()
+            formulario.save_m2m()
+            return redirect('curso_list')
+    else:
+
+        formulario = CursoForm()
+
+    return render(request, 'estudiante/curso_nuevo.html', {'formulario': formulario})
+
+
+def estudiante_nuevo(request):
+    if request.method == "POST":
+        formulario = EstudianteForm(request.POST)
+        if formulario.is_valid():
+            post = formulario.save(commit=False)
+            post.save()
+            return redirect('estudiante_list')
+    else:
+        form = EstudianteForm()
+    return render(request, 'estudiante/estudiante_nuevo.html', {'form': form})
