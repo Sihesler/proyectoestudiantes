39358a06343ca5bf5f7186c37584bba2e575b960
Sihesler <siheslermateo@gmail.com>
2018-11-06 21:22:02 -0600

    Finalizado

--- proyecto/settings.py
+++ proyecto/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'w735x7ajgc=e=erzhg_z^j2sla$l4o5syna^@hpgdcok1xgpy('
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['siheslermateo.pythonanywhere.com','127.0.0.1']
 
 
 # Application definition
