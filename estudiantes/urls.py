12e9afbc0a8b527e3ce59adeaa734cf38f594007
Sihesler <siheslermateo@gmail.com>
2018-11-06 20:27:20 -0600

    Proyecto Terminado

--- estudiantes/urls.py
+++ estudiantes/urls.py
@@ -1,21 +1,19 @@
 from django.conf.urls import url
 from . import views
 
-
 urlpatterns = [
-    #url(r'^$', views.lista_peliculas, name ='lista_peliculas'),
+    #URL de estudiantes
     url(r'^$', views.index, name='index'),
     url(r'^listar/estudiante/$', views.estudiante_list, name='estudiante_list'),
-    url(r'^listar_curso$', views.curso_list, name='curso_list'),
-    url(r'^nuevo$', views.curso_nuevo, name='curso_nuevo'),
     url(r'^nuevo_estudiante$', views.estudiante_nuevo, name='estudiante_nuevo'),
     url(r'^estudiante/(?P<pk>[0-9]+)/$', views.estudiante_detalle, name='estudiante_detalle'),
-    url(r'^curso/(?P<pk>[0-9]+)/$', views.curso_detalle, name='curso_detalle'),
     url(r'^estudiante/(?P<pk>\d+)/remove/$', views.estudiante_remove, name='estudiante_remove'),
-    url(r'^curso/(?P<pk>\d+)/remove/$', views.curso_remove, name='curso_remove'),
-    url(r'^curso/(?P<pk>[0-9]+)/editar/$', views.curso_editar, name='curso_editar'),
     url(r'^estudiante/(?P<pk>[0-9]+)/editar/$', views.estudiante_editar, name='estudiante_editar'),
 
-    #url(r'^detalle$', views.curso_detalle, name='curso_detalle'),
-    #url(r'^listar$', views.curso_list, name='curso_list'),
+    #URL de Curso
+    url(r'^listar_curso$', views.curso_list, name='curso_list'),
+    url(r'^nuevo$', views.curso_nuevo, name='curso_nuevo'),
+    url(r'^curso/(?P<pk>[0-9]+)/$', views.curso_detalle, name='curso_detalle'),
+    url(r'^curso/(?P<pk>\d+)/remove/$', views.curso_remove, name='curso_remove'),
+    url(r'^curso/(?P<pk>[0-9]+)/editar/$', views.curso_editar, name='curso_editar'),
 ]
--- estudiantes/views.py
+++ estudiantes/views.py
@@ -10,22 +10,50 @@ from django.contrib.auth.decorators import login_required
 def index(request):
     return render(request, 'estudiante/index.html')
 
+#--------Viws de estudiante----------------
+def estudiante_list(request):
+    estudiante = Estudiante.objects.all()
+    return render(request, 'estudiante/listar_estudiante.html', {'estudiante':estudiante})
+
+@login_required
+def estudiante_nuevo(request):
+    if request.method == "POST":
+        formulario = EstudianteForm(request.POST)
+        if formulario.is_valid():
+            post = formulario.save(commit=False)
+            post.save()
+            return redirect('estudiante_list')
+    else:
+        form = EstudianteForm()
+    return render(request, 'estudiante/estudiante_nuevo.html', {'form': form})
+
 @login_required
 def estudiante_detalle(request, pk):
         estudiante = get_object_or_404(Estudiante, pk=pk)
         return render(request, 'estudiante/estudiante_detalle.html', {'estudiante': estudiante})
 
 @login_required
-def curso_detalle(request, pk):
-        curso = get_object_or_404(Curso, pk=pk)
-        return render(request, 'estudiante/curso_detalle.html', {'curso': curso})
+def estudiante_remove(request, pk):
+    estudiante = get_object_or_404(Estudiante, pk=pk)
+    estudiante.delete()
+    return redirect('estudiante_list')
+
+@login_required
+def estudiante_editar(request, pk):
+    estudiante = get_object_or_404(Estudiante, pk=pk)
+    if request.method == "POST":
+        formulario = EstudianteForm(request.POST, instance=estudiante)
+        if formulario.is_valid():
+            estudiante_edit = formulario.save(commit=False)
+            estudiante_edit.save()
+            return redirect('estudiante_detalle', pk=estudiante_edit.pk)
+    else:
+        form = EstudianteForm(instance=estudiante)
+        return render(request, 'estudiante/estudiante_editar.html', {'form': form})
 
 
-def estudiante_list(request):
-    estudiante = Estudiante.objects.all()
-    return render(request, 'estudiante/listar_estudiante.html', {'estudiante':estudiante})
 
-
+#--------Viws de Curso----------------
 def curso_list(request):
     curso = Curso.objects.all()
     return render(request, 'estudiante/listar_cursos.html', {'curso':curso})
@@ -44,22 +72,9 @@ def curso_nuevo(request):
     return render(request, 'estudiante/curso_nuevo.html', {'formulario': formulario})
 
 @login_required
-def estudiante_nuevo(request):
-    if request.method == "POST":
-        formulario = EstudianteForm(request.POST)
-        if formulario.is_valid():
-            post = formulario.save(commit=False)
-            post.save()
-            return redirect('estudiante_list')
-    else:
-        form = EstudianteForm()
-    return render(request, 'estudiante/estudiante_nuevo.html', {'form': form})
-
-@login_required
-def estudiante_remove(request, pk):
-    estudiante = get_object_or_404(Estudiante, pk=pk)
-    estudiante.delete()
-    return redirect('estudiante_list')
+def curso_detalle(request, pk):
+        curso = get_object_or_404(Curso, pk=pk)
+        return render(request, 'estudiante/curso_detalle.html', {'curso': curso})
 
 @login_required
 def curso_remove(request, pk):
@@ -80,16 +95,3 @@ def curso_editar(request, pk):
     else:
         form = CursoForm(instance=curso)
     return render(request, 'estudiante/curso_editar.html', {'form': form})
-
-@login_required
-def estudiante_editar(request, pk):
-    estudiante = get_object_or_404(Estudiante, pk=pk)
-    if request.method == "POST":
-        formulario = EstudianteForm(request.POST, instance=estudiante)
-        if formulario.is_valid():
-            estudiante_edit = formulario.save(commit=False)
-            estudiante_edit.save()
-            return redirect('estudiante_detalle', pk=estudiante_edit.pk)
-    else:
-        form = EstudianteForm(instance=estudiante)
-        return render(request, 'estudiante/estudiante_editar.html', {'form': form})
